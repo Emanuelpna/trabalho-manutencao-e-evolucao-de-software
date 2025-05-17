a8926723b404099945e8f2ce754130618f7d0ba9
emanuelpna <emanuelpna@yahoo.com.br>
2025-05-17 15:17:56 -0300

    bump laravel version to 9.0 and update dependencies

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -13,42 +13,46 @@ use Illuminate\Support\Facades\DB;
 use Auth;
 use SegWeb\FileResults;
 
-class FileController extends Controller {
-    public function index() {
+class FileController extends Controller
+{
+    public function index()
+    {
         return view('index', ['file_content' => null, 'originalname' => null]);
     }
 
-    public function getJsonTerms() {
+    public function getJsonTerms()
+    {
         $json_file = Storage::disk('local')->get('terms/terms.json');
         return json_decode($json_file, true);
     }
 
-    public function submitFile(Request $request) {
+    public function submitFile(Request $request)
+    {
         $msg = [
             'text' => 'Successfully Submitted!',
             'type' => 'success'
         ];
-        if($request->file('file')->getClientMimeType() == 'application/x-php') {
-            if(Auth::check()) {
+        if ($request->file('file')->getClientMimeType() == 'application/x-php') {
+            if (Auth::check()) {
                 $user = Auth::user();
                 $user_id = $user->id;
             } else {
                 $user_id = 0;
             }
-            
+
             $file = new File();
             $file->user_id = $user_id;
             $file->file_path = $request->file('file')->store('uploads', 'local');
             $file->original_file_name = $request->file('file')->getClientOriginalName();
             $file->type = "File";
             $file->save();
-            
+
             $file_content = $this->analiseFile($file->id);
             $file_results_controller = new FileResultsController();
             return view('index', [
-                'file' => $file, 
-                'file_results' => $file_results_controller->getSingleByFileId($file->id), 
-                'file_content' => $file_content, 
+                'file' => $file,
+                'file_results' => $file_results_controller->getSingleByFileId($file->id),
+                'file_content' => $file_content,
                 'msg' => $msg
             ]);
         } else {
@@ -58,25 +62,27 @@ class FileController extends Controller {
         }
     }
 
-    public static function getFileById($id) {
+    public static function getFileById($id)
+    {
         return DB::table('files')->find($id);
     }
 
-    public function analiseFile($id_file) {
+    public function analiseFile($id_file)
+    {
         try {
             $file = $this->getFileById($id_file);
             $term = new TermController();
             $terms = $term->getTerm();
 
-            $file_location = Storage::disk('local')->getDriver()->getAdapter()->applyPathPrefix($file->file_path);
-            $fn = fopen("$file_location","r");
+            $file_location = Storage::disk('local')->path($file->file_path);
+            $fn = fopen("$file_location", "r");
             $line_number = 1;
             $file_content = NULL;
-            while(!feof($fn)) {
+            while (!feof($fn)) {
                 $file_line = fgets($fn);
 
-                foreach($terms as $term) {
-                    if(Tools::contains($term->term, $file_line)) {
+                foreach ($terms as $term) {
+                    if (Tools::contains($term->term, $file_line)) {
                         $file_results = new FileResults();
                         $file_results->file_id = $id_file;
                         $file_results->line_number = $line_number;
@@ -94,24 +100,27 @@ class FileController extends Controller {
         }
     }
 
-    public function indexYourFiles() {
+    public function indexYourFiles()
+    {
         $files = $this->getAllByUserId();
         return view('your_files', compact('files'));
     }
 
-    public function getAllByUserId() {
+    public function getAllByUserId()
+    {
         $user = Auth::user();
         return File::where('user_id', $user->id)->where('type', '<>', 'Github File')->orderByRaw('id DESC')->get();
     }
 
-    public static function getFileContentArray($id_file) {
+    public static function getFileContentArray($id_file)
+    {
         $file = DB::table('files')->find($id_file);
-        $full_file_path = base_path('storage/app/'.$file->file_path);
-        if(file_exists($full_file_path)) {
+        $full_file_path = base_path('storage/app/' . $file->file_path);
+        if (file_exists($full_file_path)) {
             $fn = fopen($full_file_path, 'r');
             $line_number = 1;
             $file_content = NULL;
-            while(!feof($fn)) {
+            while (!feof($fn)) {
                 $file_line = fgets($fn);
                 $file_content[$line_number] = $file_line;
                 $line_number++;
@@ -123,7 +132,8 @@ class FileController extends Controller {
         }
     }
 
-    public function getGithubFiles($file_id) {
+    public function getGithubFiles($file_id)
+    {
         return DB::table('files')->where('repository_id', $file_id)->get();
     }
 }
